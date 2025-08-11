bd510f5a35bbc3c1941cbef4947ef38a491325fe
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-08-11 21:19:11 +0430

    fix: the feedback data-type

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -18,7 +18,7 @@ const Resume = () => {
   const { id } = useParams();
   const [imageUrl, setImageUrl] = useState("");
   const [resumeUrl, setResumeUrl] = useState("");
-  const [feedback, setFeedback] = useState<Feedback | null>();
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -47,7 +47,7 @@ const Resume = () => {
       const imageUrl = URL.createObjectURL(imageBlob);
       setImageUrl(imageUrl);
 
-      setFeedback(data.feedback);
+      setFeedback(JSON.parse(data.feedback));
     };
 
     loadResume();
@@ -88,8 +88,8 @@ const Resume = () => {
               className={"flex flex-col gap-8 animate-in fade-in duration-1000"}
             >
               <Summary feedback={feedback}/>
-              <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
-              <Details  feedback={feedback} />
+              {/*<ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />*/}
+              {/*<Details  feedback={feedback} />*/}
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" alt="" className={"w-full"} />
