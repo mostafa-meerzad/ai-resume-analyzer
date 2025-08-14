3cf95d207dcd83024f42aa3438ff964cac06b2e6
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-08-14 16:24:28 +0430

    add: inter font-family

--- app/root.tsx
+++ app/root.tsx
@@ -34,6 +34,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
