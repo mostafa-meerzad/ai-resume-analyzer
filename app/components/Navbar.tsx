8952e7f7de5753eadcab0b8703251c43d0d39acc
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-08-15 12:57:27 +0430

    refactor: update navbar to show username and login/logout button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,29 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 const Navbar = () => {
-    return (
-        <nav className={"navbar"}>
-            <Link to={"/"} className={" text-2xl font-bold text-gradient"}>Resumind</Link>
-            <Link to={"/upload"} className={" primary-button w-fit"}>Upload Resume</Link>
-        </nav>
-    )
-}
-export default Navbar
+  const { auth, isLoading } = usePuterStore();
+  return (
+    <nav className={"navbar"}>
+      <Link to={"/"} className={" text-xl font-bold text-gradient"}>
+        Resumind
+      </Link>
+      <div className={"flex items-center gap-4"}>
+        {auth.isAuthenticated && (
+          <p
+            className={
+              " text-lg text-gray-500 overflow-hidden text-ellipsis whitespace-nowrap max-w-[130px] md:max-w-[220px]"
+            }
+            onClick={auth.signOut}
+          >
+            {auth.user?.username}
+          </p>
+        )}
+        <Link to={"/auth"} className={" secondary-button w-fit py-1"}>
+          {auth.isAuthenticated ? "Sign Out" : "Sign In"}
+        </Link>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
