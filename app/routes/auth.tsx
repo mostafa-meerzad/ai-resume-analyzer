2bf00532ceef89c8703df4ec1d4cd34f09cd0448
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-08-15 20:16:54 +0430

    refactor: auth page, and include a link to puter.com

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,5 +1,6 @@
 import { usePuterStore } from "~/lib/puter";
 import { Link, useLocation, useNavigate } from "react-router";
+import React from "react";
 
 export const meta = () => [
   {
@@ -16,50 +17,62 @@ const Auth = () => {
   return (
     <main
       className={
-        "bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center"
+        "bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex flex-col gap-8 "
       }
     >
-      <div className="gradient-border shadow-lg">
-        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
-          <div className="flex flex-col items-center gap-2 text-center">
-            <h1 className={"!font-semibold capitalize"}>
-              {auth.isAuthenticated ? "you're signed in" : "get started now"}
-            </h1>
-            <h2 className={"font-medium !text-gray-500"}>
-              {auth.isAuthenticated
-                ? "Ready to continue your job journey"
-                : "Sign in to continue your job journey"}
-            </h2>
-          </div>
-          <div>
-            {isLoading ? (
-              <button className={"auth-button animate-pulse"}>
-                <p>Signing you in...</p>
-              </button>
-            ) : (
-              <>
-                {auth.isAuthenticated ? (
-                  <button className={"auth-button"} onClick={auth.signOut}>
-                    <p>Sign Out</p>
-                  </button>
-                ) : (
-                  <button className={"auth-button"} onClick={auth.signIn}>
-                    Sign In
-                  </button>
-                )}
-              </>
-            )}
-          </div>
-          <Link
-            to={"/"}
-            className={
-              "font-semibold text-lg text-blue-950 hover:text-gray-700 "
-            }
-          >
-            Back to home
+      <Link to={"/"} className={"back-button w-fit"}>
+        <img src="/icons/back.svg" alt="back" className={"size-2.5"} />
+        <span className="text-gray-800 text-sm font-semibold">
+          Back to Homepage
+        </span>
+      </Link>
+      {/*<div className="gradient-border shadow-lg border-2 border-red-500 w-full h-[100vh]">*/}
+      <section className="flex flex-col items-center justify-center gap-8 bg-white rounded-2xl pt-20 pb-10">
+        <div className="flex flex-col items-center gap-7 text-center">
+          <h1 className={" capitalize px-4"}>
+            {auth.isAuthenticated ? "you're signed in" : "get started now"}
+          </h1>
+          <h2 className={"font-medium !text-gray-500"}>
+            {auth.isAuthenticated
+              ? "Ready to continue your job journey"
+              : "Sign in to continue your job journey"}
+          </h2>
+        </div>
+        {/*<div>*/}
+        <div className={"mt-3"}>
+          {isLoading ? (
+            <button className={"auth-button animate-pulse"}>
+              <p>Signing you in...</p>
+            </button>
+          ) : (
+            <>
+              {auth.isAuthenticated ? (
+                <button className={"auth-button"} onClick={auth.signOut}>
+                  <p>Sign Out</p>
+                </button>
+              ) : (
+                <button className={"auth-button"} onClick={auth.signIn}>
+                  Sign In
+                </button>
+              )}
+            </>
+          )}
+        </div>
+
+        <div
+          className="flex flex-col justify-center items-center mt-20 text-center
+          px-10 "
+        >
+          <p className={"text-gray-600"}>
+            Your AI credits are provided by Puter. Track your usage or top up
+            credits at puter.com.
+          </p>
+          <Link to={"https://puter.com"} className={"text-blue-700"}>
+            puter.con
           </Link>
-        </section>
-      </div>
+        </div>
+      </section>
+      {/*</div>*/}
     </main>
   );
 };
