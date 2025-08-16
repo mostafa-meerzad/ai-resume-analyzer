f4d62d8ab2be69a817f152eee57335bd9e332be7
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-08-16 19:56:52 +0430

    add: react-hot-toast

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 import { usePuterStore } from "~/lib/puter";
 import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -46,6 +47,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Toaster />
         <script src="https://js.puter.com/v2/"></script>
 
         {children}
